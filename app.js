5f0025e5550d0a04c84addc4b7935e2aae7994cb
Musa-97-js <aliyevmusa97@gmail.com>
2026-04-29 15:50:07 +0000

    jsDom

--- app.js
+++ app.js
@@ -1,54 +1,19 @@
 "use strict";
 $(document).ready(function () {
 
-
-
-
-    $("#me").click(function (e) {
+    $('.btn').click(function (e) {
         e.preventDefault();
-        function myel(myclass = "") {
-            if (!$.trim($("#inputme").val()) == '') {
-                let vall = $("#inputme").val()
-                    , newH3 = $('<h3> </h3>')
-                    , newDiv = $('<div class="line" > </div>')
-                    , bigDiv = $(`<div ${myclass} > </div>`)
-                $(newH3).append(vall)
-                $(newDiv).append(newH3)
-                $(bigDiv).append(newDiv)
-                return bigDiv
+        if (!$('input').val().trim() == '') {
+            let a = b => {
+                for (let i = 1; i <= b; i++) { $('.minibox').append(`<h2>${i}</h2>`); }
             }
-            else {
-                return
-            }
+            let v = f => Number(f) ? a($('input').val()) : $('.minibox').append(`<h2>${f}</h2>`);
+            v($('input').val())
+
         }
-        $("#messageyou").append(myel())
-        $("#messageme").append(myel('class="end"'))
-        $("#inputme").val("")
-        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
-        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
+
+        $('input').val('')
     });
 
-     $("#you").click(function (e) {
-        e.preventDefault();
-        function myel(myclass = "") {
-            if (!$.trim($("#inputyou").val()) == '') {
-                let vall = $("#inputyou").val()
-                    , newH3 = $('<h3> </h3>')
-                    , newDiv = $('<div class="line" > </div>')
-                    , bigDiv = $(`<div ${myclass} > </div>`)
-                $(newH3).append(vall)
-                $(newDiv).append(newH3)
-                $(bigDiv).append(newDiv)
-                return bigDiv
-            }
-            else {
-                return
-            }
-        }
-        $("#messageme").append(myel())
-        $("#messageyou").append(myel('class="start"'))
-        $("#inputyou").val("")
-        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageme").scrollHeight)
-        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
-    });
+
 });
